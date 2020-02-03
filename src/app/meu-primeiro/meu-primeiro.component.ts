d5bc447b68ed4059b0d183575768aa287550afe1
gabrielgs44 <gabriel.gomes.silva.999@gmail.com>
2020-02-03 15:15:29 -0300

    estudo e criacao de module

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <P>Meu primeiro component com Angular!</P>
+    `
+})
+export class MeuPrimeiroComponent {}
